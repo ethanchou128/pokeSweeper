5f2aee6c2034ebd6aa818ac6553f4f2a24220771
Ethan <elc10@sfu.ca>
2021-10-19 16:22:01 -0700

    [Ethan Chou] re-implemented the auto advance function using countdown timer

--- app/src/main/java/cmpt276/as3/assignment3/MainActivity.java
+++ app/src/main/java/cmpt276/as3/assignment3/MainActivity.java
@@ -5,13 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.os.Handler;
+import android.os.CountDownTimer;
 import android.view.View;
-import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
+
 
 public class MainActivity extends AppCompatActivity {
     MediaPlayer musicPlayer;
+    CountDownTimer countdownTimer;
+    private long remainingMilliseconds = 10000;
+    private boolean tenSecondsPassed = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +34,42 @@ public class MainActivity extends AppCompatActivity {
                 Intent i = Menu.makeLaunchIntent(MainActivity.this);
                 startActivity(i);
                 stopMusicPlayer();
+                setTenSecondsPassed(false);
                 // transition animation to next activity
                 overridePendingTransition(R.anim.fadein, R.anim.fadeout);
                 finish();
             }
         });
+        //countdown timer to determine whether to advance to main menu or not.
+        //above, if the menu button is pressed, the boolean tenSecondsPassed is set to false,
+        //which does not move the program back to the main menu screen even though the menu button
+        //is pressed.
+        //video inspiration is from here: https://www.youtube.com/watch?v=zmjfAcnosS0
+        countdownTimer = new CountDownTimer(remainingMilliseconds, 1000) {
+            @Override
+            public void onTick(long l) {
+                remainingMilliseconds = l;
+            }
+            @Override
+            public void onFinish() {
+                //if ten seconds does pass without an entry, it forces the activity through to the main menu.
+                if(getTenSecondsPassed()) {
+                    Intent i = Menu.makeLaunchIntent(MainActivity.this);
+                    startActivity(i);
+                    stopMusicPlayer();
+                    overridePendingTransition(R.anim.fadein, R.anim.fadeout);
+                    finish();
+                }
+            }
+        }.start();
+    }
+
+    private void setTenSecondsPassed(boolean isPassed) {
+        this.tenSecondsPassed = isPassed;
+    }
+
+    private boolean getTenSecondsPassed() {
+        return tenSecondsPassed;
     }
 
     //the code below was adapted from the Youtube video linked:
